5b640bbbd37d1a4ab1c8022161dfadf244bd1716
JohnGutteridge <johngutteridge@virginmedia.com>
2025-04-29 14:43:21 +0100

    Update App.js

--- SPJM_LGSF_Calculator_Working/src/App.js
+++ SPJM_LGSF_Calculator_Working/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-function App() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>SPJM LGSF Cost Calculator</h1>
-      <p>This is the starter version of your React-based calculator. Full version with Firebase and branded logic is loading next.</p>
-    </div>
-  );
-}
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCdfDPxR9-B-OLP2_2631lJbNrDQg36DXU",
+  authDomain: "spjm-lgsf.firebaseapp.com",
+  projectId: "spjm-lgsf",
+  storageBucket: "spjm-lgsf.firebasestorage.app",
+  messagingSenderId: "977679195856",
+  appId: "1:977679195856:web:dfab8850d58b1c54f5c8d5",
+  measurementId: "G-7K0Y6B8WP9"
+};
 
-export default App;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
