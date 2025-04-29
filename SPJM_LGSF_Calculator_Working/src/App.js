8590c5ea6ef40cea2cc50f6c7e7d6220e43286fb
JohnGutteridge <johngutteridge@virginmedia.com>
2025-04-29 11:24:49 +0100

    Add files via upload

---
+++ SPJM_LGSF_Calculator_Working/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>SPJM LGSF Cost Calculator</h1>
+      <p>This is the starter version of your React-based calculator. Full version with Firebase and branded logic is loading next.</p>
+    </div>
+  );
+}
+
+export default App;
